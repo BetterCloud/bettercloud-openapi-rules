65f45a47ca5abd1c957b74c149df84557cf33f3f
brockreiman <brock.reiman@bettercloud.com>
2022-07-08 10:47:19 -0400

    DEVX-144 hooks cleanup

--- test/schema-parameters-must-have-meaningful-id-names.test.js
+++ test/schema-parameters-must-have-meaningful-id-names.test.js
@@ -11,55 +11,51 @@ describe(ruleKeyUnderTest, () => {
 
   it("should not return any results for a valid input", async () => {
     const res = await spectral.run({
-                                     openapi: "3.0.0",
-                                     components: {
-                                       "schemas": {
-                                         "Pet": {
-                                           "type": "object",
-                                           "required": [
-                                             "breedId"
-                                           ],
-                                           "properties": {
-                                             "breedId": {
-                                               "type": "integer",
-                                               "format": "int64"
-                                             }
-                                           }
-                                         }
-                                       }
-                                     }
-                                   });
+      openapi: "3.0.0",
+      components: {
+        schemas: {
+          Pet: {
+            type: "object",
+            required: ["breedId"],
+            properties: {
+              breedId: {
+                type: "integer",
+                format: "int64",
+              },
+            },
+          },
+        },
+      },
+    });
 
     expect(res).toEqual([]);
   });
 
   it("should return an error if a schema defines an `id` property", async () => {
     const res = await spectral.run({
-                                     openapi: "3.0.0",
-                                     components: {
-                                       "schemas": {
-                                         "Pet": {
-                                           "type": "object",
-                                           "required": [
-                                             "id"
-                                           ],
-                                           "properties": {
-                                             "id": {
-                                               "type": "integer",
-                                               "format": "int64"
-                                             }
-                                           }
-                                         }
-                                       }
-                                     }
-                                   });
+      openapi: "3.0.0",
+      components: {
+        schemas: {
+          Pet: {
+            type: "object",
+            required: ["id"],
+            properties: {
+              id: {
+                type: "integer",
+                format: "int64",
+              },
+            },
+          },
+        },
+      },
+    });
     expect(res).toBeDefined();
     expect(res[0]).toMatchObject({
-                                   code: ruleKeyUnderTest,
-                                   message:
-                                     "Meaningful schema ids must be used in the form of {entity}Id. ie customerId, betterCloudUserId",
-                                   path: ["components", "schemas", "Pet", "required", "0"],
-                                   severity: 0,
-                                 });
+      code: ruleKeyUnderTest,
+      message:
+        "Meaningful schema ids must be used in the form of {entity}Id. ie customerId, betterCloudUserId",
+      path: ["components", "schemas", "Pet", "required", "0"],
+      severity: 0,
+    });
   });
 });
